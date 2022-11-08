f68b3165abf245d177e12415a20a9aaea5242a0b
meeia <102783643+cnmeeia@users.noreply.github.com>
2022-11-08 08:50:15 +0800

    Update ss.sh

--- rust-ss/ss.sh
+++ rust-ss/ss.sh
@@ -13,7 +13,7 @@ if type docker-compose >/dev/null 2>&1; then
     echo "docker-compose 已经安装"
 else
     echo "安装 docker-compose"
-    curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
 
@@ -21,16 +21,15 @@ if docker images | grep ss-rust >/dev/null 2>&1; then
     echo "ss-rust 镜像已经存在"
 else
     echo "拉取 ss-rust 镜像"
-    docker pull zhangguanzhang/ss-rust
+    docker pull teddysun/shadowsocks-rust
 fi
-
 
 if [ -f /etc/ss-rust/config.json ]; then
     echo "配置文件已经存在"
 else
     echo "创建配置文件"
     mkdir -p /etc/ss-rust
-    cat > /etc/ss-rust/config.json <<EOF
+    cat >/etc/ss-rust/config.json <<EOF
 {
     "server":"0.0.0.0",
     "server_port":9099,
@@ -50,7 +49,7 @@ if docker ps | grep ss-rust >/dev/null 2>&1; then
     docker restart ss-rust
 else
     echo "启动 ss-rust"
-    docker run -d --name ss-rust --restart=always -p 9099:9099 -v /etc/shadowsocks-rust:/etc/shadowsocks-rust shadowsocks/shadowsocks-rust
+    docker run -d -p 9099:9099 -p 9099:9099/udp --name ss-rust --restart=always -v /etc/shadowsocks-rust:/etc/shadowsocks-rust teddysun/shadowsocks-rust
 fi
 
-echo "安装完成 🎉 🎉 🎉 "
+echo " 安装完成 🎉 🎉 🎉 "
