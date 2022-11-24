50c9f914e9009820f29ca91395db9cd10f22775c
meeia <102783643+cnmeeia@users.noreply.github.com>
2022-11-24 18:30:44 +0800

    Update ss-rust.sh

--- all/ss-rust.sh
+++ all/ss-rust.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 if type docker >/dev/null 2>&1; then
-    echo
     echo "docker 已经安装 🎉 "
     echo
 else
@@ -73,7 +72,7 @@ echo
 echo "ss = ss, $(curl https://api.my-ip.io/ip -s), $(cat /etc/shadowsocks-rust/config.json | jq -r '.server_port'), encrypt-method=$(cat /etc/shadowsocks-rust/config.json | jq -r '.method'),password=$(cat /etc/shadowsocks-rust/config.json | jq -r '.password'),udp-relay=true"
 echo
 echo "=============================="
-echo
+
 echo " 安装完成 🎉 🎉 🎉 "
 
 echo
