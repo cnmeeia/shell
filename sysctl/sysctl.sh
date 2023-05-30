dc0dac4014d2562f68fdfb08a45a64a5cbfa49a0
meeia <102783643+cnmeeia@users.noreply.github.com>
2023-05-30 20:49:31 +0800

    Create sysctl.sh

---
+++ sysctl/sysctl.sh
@@ -0,0 +1,31 @@
+
+#!/usr/bin/env bash
+
+cat > /etc/sysctl.conf <<EOF
+
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_ecn=2
+net.ipv4.tcp_ecn_fallback = 1
+net.ipv4.tcp_frto=0
+net.ipv4.tcp_mtu_probing=0
+net.ipv4.tcp_rfc1337=1
+net.ipv4.tcp_sack=1
+net.ipv4.tcp_fack=1
+net.ipv4.tcp_window_scaling=1
+net.ipv4.tcp_adv_win_scale=2
+net.ipv4.tcp_moderate_rcvbuf=1
+net.ipv4.tcp_rmem=4096 65536 16777216
+net.ipv4.tcp_wmem=4096 65536 16777216
+net.core.rmem_max=16777216
+net.core.wmem_max=16777216
+net.ipv4.udp_rmem_min=8192
+net.ipv4.udp_wmem_min=8192
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+net.ipv4.conf.all.route_localnet=1
+net.ipv4.ip_forward=1
+net.ipv4.conf.all.forwarding=1
+net.ipv4.conf.default.forwarding=1
+EOF
+
+sysctl -p 
