dd3f2ca83719c8fb8c862bd5e6cdbb4bc86b50e3
meeia <102783643+cnmeeia@users.noreply.github.com>
2023-08-14 12:36:13 +0800

    Create deep.sh

---
+++ deepl/deep.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo
+mkdir -p $PWD/deepl
+echo
+echo $PWD/deepl
+echo
+cd $PWD/deepl
+echo
+cat >$PWD/docker-compose.yaml <<EOL
+version: "3.8"
+services:
+  deepl:
+    restart: always
+    ports:
+      - "12311:80"
+    container_name: deepl
+    image: "mick2019/deepl:latest"
+EOL
+echo
+cd $PWD/deepl && docker-compose up -d
+echo
