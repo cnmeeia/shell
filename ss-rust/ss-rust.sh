5609fea2bc5c94e02e90c0345fb8dc5fb1ef6aff
meeia <102783643+cnmeeia@users.noreply.github.com>
2023-10-07 19:28:44 +0800

    Update ss-rust.sh

--- ss-rust/ss-rust.sh
+++ ss-rust/ss-rust.sh
@@ -23,7 +23,7 @@ if type docker-compose >/dev/null 2>&1; then
     echo
 else
     echo "安装 docker-compose"
-    curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    curl -L https://github.com/docker/compose/releases/download/2.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
 echo
