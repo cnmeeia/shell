dcd64c69dc2da29e21c6ac3fb87b52b81b8902ad
meeia <102783643+cnmeeia@users.noreply.github.com>
2023-10-07 19:16:19 +0800

    Update deep.sh

--- deepl/deep.sh
+++ deepl/deep.sh
@@ -6,6 +6,15 @@ echo $PWD/deepl
 echo
 cd $PWD/deepl
 echo
+if type docker-compose >/dev/null 2>&1; then
+    echo "docker-compose 已经安装 🎉 "
+    echo
+else
+    echo "安装 docker-compose"
+    curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+fi
+echo
 cat >$PWD/deepl/docker-compose.yaml <<EOL
 version: "3.8"
 services:
