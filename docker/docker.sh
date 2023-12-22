732cc8aaa71ab1a82c88c15f5bba6c5ea99f7ca8
meeia <102783643+cnmeeia@users.noreply.github.com>
2023-12-22 17:00:31 +0800

    Update docker.sh

--- docker/docker.sh
+++ docker/docker.sh
@@ -9,3 +9,10 @@ echo
 echo "设置自启动"
 
 systemctl enable docker && systemctl start docker
+
+echo 
+
+echo "安装 docker-compose"
+
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose  && chmod +x /usr/local/bin/docker-compose  && docker-compose --version
+
